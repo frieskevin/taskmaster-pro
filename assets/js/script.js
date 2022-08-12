7240a686d8effa622840a4e5fa3b7b0c1c8b2ebb
alotofsnakes <mrfries@gmail.com>
2022-08-11 19:22:43 -0700

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -119,16 +119,18 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function(event) {
-    console.log('actiave', this);
+    $(this).addClass('dropover');
+    $('.bottom-trash').addClass('bottom-trash-drag');
   },
   deactivate: function(event) { 
-     console.log('deactivate', this);
+    $(this).removeClass('dropover');
+    $('.bottom-trash').removeClass('bottom-trash-drag');
   },
   over: function(event) {
-    console.log('over', event.target);
+    $(event.target).addClass('dropover-active');
   },
   out: function(event) {
-    console.log('out', event.target);
+    $(event.target).removeClass('dropover-active');
   },
   update: function(event) {
     var tempArr = [];
@@ -160,12 +162,13 @@ $("#trash").droppable({
   tolerance: "touch",
   drop: function(event, ui) {
     ui.draggable.remove();
+    $('.bottom-trash').removeClass('bottom-trash-active');
   },
   over: function(event, ui) {
-    console.log('over');
+    $('.bottom-trash').addClass('bottom-trash-active');
   },
   out: function(event, ui) {
-    console.log('out');
+    $('.bottom-trash').removeClass('bottom-trash-active');
   }
 });
 
@@ -197,7 +200,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -229,5 +232,11 @@ $("#remove-tasks").on("click", function() {
 
 // load tasks for the first time
 loadTasks();
+
+setInterval(function() {
+  $(".card .list-group-item").each(function(index, el) {
+    auditTask(el);
+  });
+}, 1800000);
 
 
